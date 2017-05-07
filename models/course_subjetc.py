c19e589f91f72a3292e81bbbc382a6549193af1d
drohnet <drohne@gmail.com>
2017-05-07 19:26:02 +0200

    Fixed related field api 8

--- models/course_subjetc.py
+++ models/course_subjetc.py
@@ -6,17 +6,17 @@ class course_subject(models.Model):
     _name = 'course.subjectv8'
     
     name=fields.Char('Name', required=True, size=64,ondelete='cascade')
-    course_id=fields.Char('Name', required=True, size=64,ondelete='cascade')
+    course_id=fields.Many2one('course', string='Course', required=True, ondelete='cascade')
     #fix it
-    course_id_time_table_id=fields.Related('course_id','time_table_id_detail_ids',type='many2one' ,relation='time.table', string='Course time table')
-    time_table_id_detail_ids=fields.Related('course_id','time_table_id', 'time_detail_ids', type='one2many', relation='time.table.detail', string='Time table detail')
+    course_id_time_table_id=fields.Char(related='course_id.time_table_id_detail_ids', string='Course time table')
+    time_table_id_detail_ids=fields.Char('course_id.time_table_id.time_detail_ids')
     subject_id=fields.Many2one('subject', string='Subject', required=True, ondelete='restrict')
     test_id=fields.Many2one('subject', string='test_id',  ondelete='cascade')
     child_ids=fields.One2many('course.subject.time.table.detail', 'course_subject_id', string='Time table', ondelete='cascade')
     #fix it
-    test=fields.Related('course_id','time_table_id','time_detail_ids',type="one2many",relation='time.table.detail' ,string='test1')
+    test=fields.Char(related='course_id.time_table_id.time_detail_ids',string='test1')
     #fix it
-    testt=fields.Related('course_id','time_table_id', 'time_detail_ids', type='one2many', relation='time.table.detail', string='Testt2')
+    testt=fields.Char(related='course_id.time_table_id.time_detail_ids', string='Testt2')
     #fix it
-    testtt=fields.Related('course','time_table_id_detail_ids','day_of_week' ,string="testtt3")
+    testtt=fields.Char('course.time_table_id_detail_ids.day_of_week' )
    
