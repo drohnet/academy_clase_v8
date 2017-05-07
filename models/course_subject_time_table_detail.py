ce85f7a4b2582039675363b723fca04f38b78672
drohnet <drohne@gmail.com>
2017-05-07 16:22:25 +0200

    course.subject.time.table.detail basic model api 8

---
+++ models/course_subject_time_table_detail.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from openerp import models, fields, api, _, exceptions
+
+"""curso-materia-horario-detalle"""    
+class course_subject_time_table_detail(models.Models):    
+    """ Course subject time table detail  """
+    _name = 'course.subject.time.table.detail'
+    
+    name=fields.Char('Name', required=True, size=16,ondelete='cascade')
+    sequence=fields.Integer('Sequence',ondelete='cascade')
+    course_subject_id=fields.Many2one('course.subject', 'Course-Subject relation', required=True,ondelete='cascade')
+    day_of_week=fields.selection(DAYS_OF_WEEK, 'Days of week', required=True,ondelete='cascade')
+    hour_start=fields.Float('From',digits=(4,2), required=True, help='Hour from',ondelete='cascade')
+    hour_end=fields.Float('To', digits=(4,2), required=True, help='Hour to',ondelete='cascade')
+ 
