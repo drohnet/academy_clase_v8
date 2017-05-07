a68452f4844b572ae2176973ef9c9bf20c2e1154
drohnet <drohne@gmail.com>
2017-05-07 16:53:39 +0200

    Init whit all class updated

--- models/__init__.py
+++ models/__init__.py
@@ -1,2 +1,10 @@
 # -*- coding: utf-8 -*- 
-import models
+import course
+import course_subject_time_table_detail
+import course_subject
+import res_partner
+import student_attendance
+import student_attendance_detail
+import subject
+import time_table
+import time_table_detail
