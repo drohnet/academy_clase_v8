e256c3473eddc5f44bade39b6c6afabd271f1c87
drohnet <drohne@gmail.com>
2017-05-07 16:07:38 +0200

    subject model basic data

--- models/subject.py
+++ models/subject.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from openerp import models, fields, api, _, exceptions
 
-class subject(osv.osv):    
+class subject(models.Model):    
     """ Subject  """
     _name = 'subject'
     
