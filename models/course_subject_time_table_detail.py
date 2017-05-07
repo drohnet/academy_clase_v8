a5bde6b6463a5582bb4aceedd049eb410aa7b350
drohnet <drohne@gmail.com>
2017-05-07 19:04:44 +0200

    fix sintaxis line 20

--- models/course_subject_time_table_detail.py
+++ models/course_subject_time_table_detail.py
@@ -17,7 +17,7 @@ class course_subject_time_table_detail(models.Model):
     name=fields.Char('Name', required=True, size=16,ondelete='cascade')
     sequence=fields.Integer('Sequence',ondelete='cascade')
     course_subject_id=fields.Many2one('course.subject', 'Course-Subject relation', required=True,ondelete='cascade')
-    day_of_week=fields.selection(DAYS_OF_WEEK, 'Days of week', required=True,ondelete='cascade')
+    day_of_week=fields.Selection(DAYS_OF_WEEK, 'Days of week', required=True,ondelete='cascade')
     hour_start=fields.Float('From',digits=(4,2), required=True, help='Hour from',ondelete='cascade')
     hour_end=fields.Float('To', digits=(4,2), required=True, help='Hour to',ondelete='cascade')
  
