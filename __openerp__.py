4bfb8b0409f18e4c4192c3c35d058015ca4c5a0a
drohnet <drohne@gmail.com>
2017-05-07 14:47:00 +0200

    subo archivos base para el modulo

---
+++ __openerp__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "academy api 8",
+    'version': '8.0.1.0.0',
+    'author': "Konectia people",
+    'summary': "Academy upgraded from academy v7",
+    'contributors': [
+        'Drohnet <drohne@gmail.com>',
+    ],
+    'website': "http://www.none.com",
+    'category': "Training",
+    'depends': ['base'],
+    'data': [
+        
+        
+    ],
+}
