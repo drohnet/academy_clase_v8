10a255f5be1ec24a091578c212011d21f7b00aac
drohnet <drohne@gmail.com>
2017-05-14 10:39:18 +0200

    logic upgraded to Api 8

--- models/res_partner.py
+++ models/res_partner.py
@@ -8,10 +8,43 @@ ACADEMY_TYPE_LIST = [('public','Public'),
 """Herencia: Academias, Estudiantes, Profesores ->Tipo"""
 class res_partner(models.Model):    
     _inherit = 'res.partner'
-
+    
     academy_type=fields.Selection(ACADEMY_TYPE_LIST, 'Academy type', help="Select the academy type for the record")
     course_ids=fields.One2many('coursev8', 'academy_id', string='Courses')
     is_academy=fields.Boolean(string='Is Academy')
     is_student=fields.Boolean(string='Is student')
     is_teacher=fields.Boolean(string='Is teacher')
+    
+    @api.multi
+    @api.constrains("name")
+    def _check_name(self):
+        
+        if self.is_teacher == True :
+            previous_name_ids = self.search( [('id','!=',self.id),('name','=ilike',self.name),('is_teacher','=',True)])
+            if previous_name_ids.name == self.name:
+                raise exceptions.ValidationError(
+                        _("Try enter other Teacher name !! This is taken."))
+                return
+                
+        if self.is_academy == True :
+            previous_name_ids = self.search( [('id','!=',self.id),('name','=ilike',self.name),('is_academy','=',True)])
+            if previous_name_ids.name == self.name:
+                raise exceptions.ValidationError(
+                        _("Try enter other Academy name !! This is taken."))
+                return
+                
+        if self.is_student == True :
+            previous_name_ids = self.search( [('id','!=',self.id),('name','=ilike',self.name),('is_student','=',True)])
+            if previous_name_ids.name == self.name:
+                raise exceptions.ValidationError(
+                        _("Try enter other Student name !! This is taken."))
+                return
+
+    @api.multi
+    @api.onchange("name")
+    def onchange_name(self):
+        if self.name:
+            self.name = self.name.title()
+        return 
+   
  
