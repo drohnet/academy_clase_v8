321f1efe2a2f2558f1cac1852169d136cc949871
drohnet <drohne@gmail.com>
2017-05-14 17:30:06 +0200

    states and related field fix

--- models/course.py
+++ models/course.py
@@ -26,8 +26,47 @@ class course(models.Model):
     student_ids=fields.Many2many('res.partner','course_student_table',string='Students', domain=[('is_student','=',True)])
     teacher_ids=fields.Many2many('res.partner','course_teacher_table',string='Teachers', domain=[('is_teacher','=',True)])
     time_table_id=fields.Many2one('time.tablev8', string='Time table', required=True, ondelete='restrict')
-    #~ time_table_id_detail_ids=fields.Char(related='time_table_id.time_detail_ids', string='Time table detail')
+    time_table_id_detail_ids=fields.One2many('time.table.detailv8','name',string="test", compute="time_details")
  
     _defaults = {
             'state': COURSE_STATE[0][0],
     }
+    
+    @api.multi
+    def time_details(self):
+        time_table_det = self.env['time.tablev8'].browse(self.time_table_id.id)
+        self.time_table_id_detail_ids = time_table_det.time_detail_ids
+        return
+    
+    @api.multi
+    @api.onchange("time_table_id")
+    def change_change(self):
+        if self.time_table_id:
+            self.time_details()
+            return
+        return
+        
+    @api.multi    
+    def button_draft(self):
+        self.state= 'draft'
+        return True
+     
+    @api.multi        
+    def button_confirmed(self):
+        self.state = 'confirmed'
+        return True
+    
+    @api.multi
+    def button_cancel(self):
+        self.state= 'cancel'
+        return True
+        
+    @api.multi
+    def button_done(self):
+        self.state= 'done'
+        return True
+        
+    @api.multi
+    def button_in_process(self):
+        self.state= 'in_process'
+        return True
